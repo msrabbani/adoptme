af79544733f1fa14337886f179f6dac49961d112
msrabbani <msrabbanii@gmail.com>
2018-12-23 19:16:00 +0700

    resusable component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-// your code goes here!
+const Pet = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Anim'),   
+        React.createElement('h2', {}, 'Bagoy'),   
+        React.createElement('h2', {}, 'Cerry'),   
+        React.createElement('h2', {}, 'Deblo'),   
+        React.createElement('h2', {}, 'Elisa')   
+    ])
+}
+
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Adopt Me!'),
+  return React.createElement('div', {},
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet)
   );
 };
 

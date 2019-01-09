0adff31aa074778f7d34b6c98cbd7bd864c9c33e
msrabbani <msrabbanii@gmail.com>
2019-01-09 19:18:24 +0700

    adding unique keys to component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
 import React from 'react';
 import {render} from 'react-dom';
-import pf from 'petfinder-client'
-import Pet from './Pet'
+import pf from 'petfinder-client';
+import Pet from './Pet';
 
 const petfinder = pf({
-    key: process.env.API_KEY,
-    secret: process.env.API_SECRET
-})
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET,
+});
 
 class App extends React.Component {
-    constructor (props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            pets: []
+    this.state = {
+      pets: [],
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({output: 'full', location: 'Nevada City, CA'})
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
         }
-    }
 
-    componentDidMount() {
-        petfinder.pet.find({ output: "full", location: "Nevada City, CA"})
-            .then(data => {
-                let pets;
-
-                if(data.petfinder.pets && data.petfinder.pets.pet) {
-                    if(Array.isArray(data.petfinder.pets.pet)) {
-                        pets = data.petfinder.pets.pet 
-                    } else {
-                        pets = [data.petfinder.pets.pet]
-                    }
-                } else {
-                    pets = []
-                }
-
-                this.setState({
-                    pets
-                })
-            })
-    }
+        this.setState({
+          pets,
+        });
+      });
+  }
 
   render() {
     return (
       <div>
-          <h1>Adopt Me!</h1>
-          {/*   <pre><code>{JSON.stringify(this.state, null, 4)}</code></pre> */}
-          { this.state.pets.map(pet => {
-              let breed
+        <h1>Adopt Me!</h1>
+        {/*   <pre><code>{JSON.stringify(this.state, null, 4)}</code></pre> */}
+        {this.state.pets.map(pet => {
+          let breed;
 
-              if(Array.isArray(pet.breeds.breed)) {
-                  breed = pet.breeds.breed.join(", ")
-              } else {
-                  breed = pet.breeds.breed
-              }
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(', ');
+          } else {
+            breed = pet.breeds.breed;
+          }
 
-              return <Pet animal= {pet.animal} name= {pet.name} breed={breed} />
-          })}
+            return <Pet 
+                key = {pet.id}
+                animal={pet.animal} 
+                name={pet.name} 
+                breed={breed} 
+            />;
+        })}
       </div>
     );
   }

e30ab0b6a2553686163960a06eb1dc1c4d3427a5
msrabbani <msrabbanii@gmail.com>
2019-01-27 17:59:45 +0700

    Using the Provider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
+import pf from 'petfinder-client'
+import {Provider} from './SearchContext'
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams"
 
+const petfinder = pf({
+    key:process.env.API_KEY,
+    secret:process.env.API_SECRET
+})
+
 class App extends React.Component {
+    constructor (props){
+        super(props)
+
+        this.state = {
+            location: "Nevada City, CA",
+            animal: "",
+            breed: "",
+            breeds: [],
+            handleAnimalChange: this.handleAnimalChange,
+            handleBreedChange: this.handleBreedChange,
+            handleLocationChange: this.handleLocationChange,
+            getBreeds: this.getBreeds
+        }
+    }
+
+    handleLocationChange = event => {
+        this.setState({
+            location: event.target.value
+        })
+    }
+
+    handleAnimalChange = event => {
+        this.setState({
+            animal: event.target.value
+        },
+            this.getBreeds
+        )
+    }
+
+    handleBreedChange = event => {
+        this.setState({
+            breed: event.target.value
+        })
+    }
+    
+    getBreeds() {
+        if (this.state.animal) {
+            petfinder.breed
+                .list({animal: this.state.animal})
+                .then(data => {
+                if (
+                    data.petfinder && 
+                    data.petfinder.breeds && 
+                    Array.isArray(data.petfinder.breeds.breed)
+                ) {
+                    this.setState({ 
+                        breeds: data.petfinder.breeds.breed
+                    })
+                } else {
+                    this.seState({ breeds: [] })
+                }
+                  
+                }).catch(console.error)
+
+        } else {
+            this.setState({breeds: [] })
+        }
+
+    }
   render() {
     return (
       <div>
         <header>
           <Link to="/">Adopt Me!</Link>
-        </header>
-
+      </header>
+      <Provider value={this.state}>
         <Router>
           <Results path="/" />
           <Details path="/details/:id" />
           <SearchParams path="/search-params" />
-        </Router>
+      </Router>
+  </Provider>
+
+
       </div>
     );
   }
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,64 +1,7 @@
 import React from 'react'
 import {ANIMALS} from 'petfinder-client'
-import pf from 'petfinder-client'
-
-const petfinder = pf({
-    key: process.env.API_KEY,
-    secret: process.env.API_SECRET
-})
 
 class SearchParams extends React.Component {
-    state = {
-        location: "Nevada City, CA",
-        animal: "",
-        breed: "",
-        breeds: []
-    }
-
-    handleLocationChange = event => {
-        this.setState({
-            location: event.target.value
-        })
-    }
-
-    handleAnimalChange = event => {
-        this.setState({
-            animal: event.target.value
-        },
-            this.getBreeds
-        )
-    }
-
-    handleBreedChange = event => {
-        this.setState({
-            breed: event.target.value
-        })
-    }
-    
-    getBreeds() {
-        if (this.state.animal) {
-            petfinder.breed
-                .list({animal: this.state.animal})
-                .then(data => {
-                if (
-                    data.petfinder && 
-                    data.petfinder.breeds && 
-                    Array.isArray(data.petfinder.breeds.breed)
-                ) {
-                    this.setState({ 
-                        breeds: data.petfinder.breeds.breed
-                    })
-                } else {
-                    this.seState({ breeds: [] })
-                }
-                  
-                }).catch(console.error)
-
-        } else {
-            this.setState({breeds: [] })
-        }
-
-    }
 
     render() {
         return (
